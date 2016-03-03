9e81200ecfa3f2747ec5dbfbcfce8a6046bfca1f
AmylNitrate <111661@student.sae.edu.au>
2016-03-03 15:42:28 +1100

    Darkness/New Scenes
    Added new scenes
    Organized folders
    New scripts:
    Darkness

---
+++ Prototype1/Assets/_scripts/Darkness.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Darkness : MonoBehaviour {
+    public Indicator scriptIndicator;
+    public GameObject _torch;
+	
+    public bool hasLight;
+
+    public AudioClip darkness;
+    public AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        scriptIndicator = GameObject.FindGameObjectWithTag("Collectable").GetComponent<Indicator>();
+
+      //find the objects
+        _torch = GameObject.FindGameObjectWithTag("torch");
+
+      //hide objects
+        _torch.gameObject.SetActive(false);
+
+        hasLight = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!audioSource.isPlaying && !hasLight)
+        {
+            audioSource.clip = darkness;
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+        if (hasLight)
+        {
+            audioSource.Stop();
+        }
+        if (scriptIndicator.torch == true)
+        {
+            _torch.gameObject.SetActive(true);
+            hasLight = true;
+        }
+
+    }
+}
