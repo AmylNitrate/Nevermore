743d26817735e386effa5302be4c05f5a3e50684
Washcloth <cjwalsh@live.com.au>
2016-03-03 14:05:03 +1100

    intro
    opening sequence and opening level started

---
+++ Prototype1/Assets/_scripts/flyCam.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flyCam : MonoBehaviour {
+
+	public Transform target;
+	public float speed;
+	void Update() {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+	}
+}
